e57b24705ea7262a7a607890f4a0ed5b204018fb
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-01 09:46:15 +0800

    fixed mason issue

--- init.lua
+++ init.lua
@@ -710,16 +710,15 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-
         'css-lsp',
         'delve',
-        'emmet-ls emmet_ls',
-        'eslint-lsp eslint',
+        'emmet-ls',
+        'eslint-lsp',
         'hadolint',
-        'lua-language-server lua_ls',
+        'lua-language-server',
         'prettier',
-        'tailwindcss-language-server tailwindcss',
-        'typescript-language-server ts_ls',
+        'tailwindcss-language-server',
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
