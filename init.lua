4814cda099da618772eaae009d3db1ca85f479ec
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-04 00:09:27 +0800

    updated config

--- init.lua
+++ init.lua
@@ -366,6 +366,12 @@ require('lazy').setup({
     opts = {},
     config = true,
   },
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
