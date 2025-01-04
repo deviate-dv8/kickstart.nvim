da7fa14d976e213a62404b945ccde237998d3792
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-05 01:39:05 +0800

    updated config

--- init.lua
+++ init.lua
@@ -192,6 +192,21 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- My custom keymaps
 
+vim.opt.colorcolumn = '80'
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -371,6 +386,43 @@ require('lazy').setup({
     opts = {},
     event = 'VeryLazy',
     enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
+  {
+    'nvim-pack/nvim-spectre',
+    event = 'VeryLazy',
+    keys = {
+      {
+        '<leader>S',
+        '<cmd>lua require("spectre").toggle()<CR>',
+        desc = 'Toggle Spectre',
+      },
+      {
+        '<leader>sw',
+        '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+        desc = 'Search current word',
+        mode = 'n',
+      },
+      {
+        '<leader>sw',
+        '<esc><cmd>lua require("spectre").open_visual()<CR>',
+        desc = 'Search current word',
+        mode = 'v',
+      },
+      {
+        '<leader>sp',
+        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+        desc = 'Search on current file',
+      },
+    },
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<c-\>]], -- Default toggle keybinding
+      direction = 'horizontal', -- Default direction (can be overridden with keymaps)
+      size = 20, -- Default size of terminal
+    },
   },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
